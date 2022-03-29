2e12cedf602b2ce83b38b838308242d56ff55e57
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2022-03-29 12:05:52 -0300

    Feature Item model for final Capstone Project

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_27_231030) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_29_145551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "reservation_id"
+    t.date "date_reservation"
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -30,4 +43,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_27_231030) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
