ad47dcc62dbed730e500dc5d7a00388d436848c1
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-04-07 04:01:47 +0200

    Add default url for swagger request

--- spec/requests/api/spaces_spec.rb
+++ spec/requests/api/spaces_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Spaces Api', type: :request do
     post 'Creates a new space' do
       tags 'Spaces'	
       consumes 'application/json'
+      parameter name: :Authorization, in: :header, type: :string, required: true
       parameter name: :space, in: :body, schema: {
         type: :object,
         properties: {
@@ -31,20 +32,10 @@ RSpec.describe 'Spaces Api', type: :request do
     get 'Retrieves all spaces' do
       tags 'Spaces'
       produces 'application/json'
+      parameter name: :Authorization, in: :header, type: :string, required: true
 
       response '200', 'spaces retrieved' do
-        schema type: :array,
-          items: {
-            type: :object,
-            properties: {
-              id: { type: :integer },
-              name: { type: :string },
-              description: { type: :string },
-              image: { type: :string },
-              price: { type: :number },
-            },
-            required: [ 'id', 'name', 'description', 'image', 'price' ]
-          }
+        let(:space) { Space.all }
         run_test!
       end
 
@@ -52,45 +43,39 @@ RSpec.describe 'Spaces Api', type: :request do
         run_test!
       end
     end
+  end
+end
 
+RSpec.describe 'Space Api', type: :request do
+  path '/spaces/{id}' do
     get 'Retrieves a space' do
       tags 'Spaces'
       produces 'application/json'
+      parameter name: :Authorization, in: :header, type: :string, required: true
       parameter name: :id, in: :path, type: :string
 
-      response '200', 'space found' do
-        schema type: :object,
-          properties: {
-            id: { type: :integer },
-            name: { type: :string },
-            description: { type: :string },
-            image: { type: :string },
-            price: { type: :number },
-          },
-          required: [ 'id', 'name', 'description', 'image', 'price' ]
-
-        let(:id) { Space.create(name: 'test name', description: 'test description', image: 'test image', price: 1).id }
+      response '200', 'space retrieved' do
+        let(:space) { Space.first }
         run_test!
       end
 
       response '404', 'space not found' do
-        let(:id) { 'invalid' }
         run_test!
       end
     end
 
     delete 'Deletes a space' do
       tags 'Spaces'
-      produces 'application/json'
+      consumes 'application/json'
       parameter name: :id, in: :path, type: :string
+      parameter name: :Authorization, in: :header, type: :string, required: true
 
-      response '204', 'space deleted' do
-        let(:id) { Space.create(name: 'test name', description: 'test description', image: 'test image', price: 1).id }
+      response '200', 'space deleted' do
+        let(:space) { Space.create(name: 'test name', description: 'test description', image: 'test image', price: 1) }
         run_test!
       end
 
       response '404', 'space not found' do
-        let(:id) { 'invalid' }
         run_test!
       end
     end
--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -1,9 +1,9 @@
 require 'swagger_helper'
 
-RSpec.describe 'Session Api', type: :request do
+RSpec.describe 'Signup Api', type: :request do
   path '/signup' do
     post 'Creates a new user' do
-      tags 'Session'
+      tags 'Sign up'
       consumes 'application/json'
       parameter name: :user, in: :body, schema: {
         type: :object,
@@ -19,13 +19,42 @@ RSpec.describe 'Session Api', type: :request do
         required: [ 'name', 'email', 'password', 'password_confirmation' ]
       }
 
-      response '201', 'user created' do
+      response '201', 'Signed up successfull' do
         let(:user) { { name: 'test name', email: 'email@gmail.com', password: 'password', password_confirmation: 'password' } }
         run_test!
       end
 
       response '422', 'invalid request' do
         let(:user) { { email: 'email@gmail.com' } }
+        run_test!
+      end
+    end
+  end
+end
+
+RSpec.describe 'Login Api', type: :request do
+  path '/login' do
+    post 'Creates a new user' do
+      tags 'Login'
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        user: {
+          type: :object,
+          properties: {
+            email: { type: :string },
+            password: { type: :string },
+          }
+        },
+        required: [ 'email', 'password' ]
+      }
+
+      response '201', 'Logged in successfull' do
+        let(:user) { { email: 'email@gmail.com', password: 'password' } }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
         run_test!
       end
     end
